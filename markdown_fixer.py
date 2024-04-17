5d71181639730534e9737cde107d0f0b6ad6f382
Maho Yoshino <62778359+Maho-Yoshino@users.noreply.github.com>
2024-04-17 08:24:45 +0200

    md_fixer quickfix

--- markdown_fixer.py
+++ markdown_fixer.py
@@ -2,15 +2,21 @@ import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 def open_file(file_dir:str):
 	with open(file_dir, encoding="utf-8") as file:
+		print("File found")
 		add_newline(file.readlines(), file_dir)
 def add_newline(text:list, file_dir:str):
 	new_content = ""
 	for i in text:
-		print(i)
+		i=str(i)
 		if i[-1:-2]!="  ":
-			new_content += i.strip()+"  \n"
+			new_content += i.removesuffix("\n")+"  \n"
+		else:
+			print(i[-1:-2])
+			new_content += i
 	with open(file_dir, "w", encoding="utf-8") as file:
 		file.write(new_content)
+		print("Done.\n")
+		_init_()
 def _init_():
 	folders = []
 	files = []
@@ -29,9 +35,14 @@ def _init_():
 			print(f"\n\nFiles:{'\n'.join(files)}")
 		file_dir = input("Enter filename/folder name: ")
 		if file_dir in folders:
-			os.chdir(os.getcwd()+file_dir)
-			thing()
+			os.chdir(os.getcwd()+f"\\{file_dir}")
+			return thing()
 		elif file_dir in files:
-			return os.getcwd()+f"\\{file_dir}.md"
-	open_file(thing())
+			result = os.path.join(os.getcwd(), file_dir + ".md")
+			return result
+		else:
+			thing()
+	file_path = thing()
+	if file_path:
+		open_file(file_path)
 _init_()
