42055a63be3c553e4786e8860c2044dc371499b8
Maho Yoshino <62778359+Maho-Yoshino@users.noreply.github.com>
2024-10-06 09:16:56 +0200

    2024.10.04

--- 2024-2025/Matek/calc.py
+++ 2024-2025/Matek/calc.py
@@ -1,6 +1,18 @@
 from math import sqrt,sin,cos,tan,log,pi
+replace_dict = {
+	"^":"**"
+}
+def root(x:int|float|str,n:int|float|str):
+	if isinstance(x, str):
+		x = float(x)
+	if isinstance(n, str):
+		n = float(n)
+	return x**(1/n)
 while True:
 	try:
-		print(str(eval(input("> ").replace("^", "**"))) + "\n")
+		equation = input("> ")
+		for item, replacant in replace_dict.items():
+			equation = equation.replace(item, replacant)
+		print(str(eval(equation)) + "\n")
 	except Exception as e:
 		print(f"error: {e}\n")
