25522261680b9fa1643cf8a7326d2b628fe5075b
Maho Yoshino <62778359+Maho-Yoshino@users.noreply.github.com>
2025-09-18 20:04:07 +0200

    2025.09.18
    Házi:
    - Matek
     - a1 = 5, a3 = 20, q = ?
     - a2 = 12, a4 = 108, q = ?, a5 = ?

---
+++ 2025-2026/AsztaliAlkFejlesztes/emelt_szint_erettsegi_1/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace erettegi_fel_emelt_1
+{
+    internal class Program
+    {
+        private static bool NoFilter<G>(G item) => true;
+        static private string Input(string txt)
+        {
+            Console.Write(txt);
+            return Console.ReadLine();
+        }
+        static private T TryInput<T>(string txt, Func<T, bool> predicate = null)
+        {
+            predicate = predicate == null ? NoFilter : predicate;
+            while (true)
+            {
+                try
+                {
+                    T[] N = new T[1] { (T)Convert.ChangeType(Input(txt).Replace('.', ','), typeof(T)) };
+                    if (N.Any(predicate))
+                    {
+                        return N[0];
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Hibás érték.");
+                    continue;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int feladat_num = int.Parse(Console.ReadLine());
+            switch (TryInput<byte>("Melyik feladatot kéri? (2, 3): ", x=> x>=2 && x<=3))
+            {
+                case 2:
+                    Console.Clear();
+                    Fel2();
+                    break;
+                case 3:
+                    Console.Clear();
+                    Fel3();
+                    break;
+            }
+            Console.Write("\n<Program vége>");
+            Console.ReadLine();
+        }
+        static void Fel2() {
+            const int N = 20;
+            int[] A = new int[N];
+            void Feltolt()
+            {
+                Random rnd = new Random();
+                for (int i = 0; i < N; i++)
+                {
+                    A[i] = rnd.Next(2, 1001);
+                }
+            }
+            void Kiir()
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    Console.WriteLine(A[i]);
+                }
+            }
+            bool Prim(int _A)
+            {
+                int I = 2;
+                while (I <= Math.Sqrt(_A) && (_A%I!=0))
+                {
+                    I++;
+                }
+                return !(I <= Math.Sqrt(_A));
+            }
+            void PrimPakol()
+            {
+                int E = 0;
+                int V = N - 1;
+                int S = A[0];
+                while (E<V)
+                {
+                    while (E<V && !(Prim(A[V])))
+                    {
+                        V -= 1;
+                    }
+                    if (E<V)
+                    {
+                        A[E] = A[V];
+                        E++;
+                        while (E<V && Prim(A[E]))
+                        {
+                            E++;
+                        }
+                        if (E<V)
+                        {
+                            A[V] = A[E];
+                            V--;
+                        }
+                    }
+                }
+                A[E] = S;
+            }
+            void Sub_Main()
+            {
+                Feltolt();
+                Kiir();
+                PrimPakol();
+                Kiir();
+            }
+            Sub_Main();
+        }
+        public class cFel3
+        {
+            public struct Point
+            {
+                public int x;
+                public int y;
+            }
+            public byte[] rgb;
+            public Point start_pos, end_pos;
+            public double SzakaszLength => Math.Sqrt((start_pos.x - end_pos.x) ^ 2 + (start_pos.y - end_pos.y) ^ 2);
+            public cFel3(string tmp)
+            {
+                byte[] line = tmp.Split(';').ToList().ConvertAll(x => byte.Parse(x)).ToArray();
+                start_pos = new Point { x=line[0], y=line[1] };
+                end_pos = new Point { x=line[2], y=line[3] };
+                rgb = new byte[] { line[4], line[5], line[6] };
+            }
+            public bool IsMirror(cFel3 a, int[] CanvasSize)
+            {
+                return (a.start_pos.y == start_pos.y && a.end_pos.y == end_pos.y &&
+                    (a.start_pos.x == (CanvasSize[0] - 1 - start_pos.x)) &&
+                    (a.end_pos.x == (CanvasSize[0] - 1 - end_pos.x)));
+            }
+        }
+        static void Fel3()
+        {
+            string[] lines = File.ReadAllLines(Input("Adja meg a fájl nevét: "));
+            int[] sizes = lines[0].Split(';').ToList().ConvertAll(x => int.Parse(x)).ToArray();
+            cFel3[] adat = new cFel3[lines.Length - 1];
+            for (int i = 0; i < lines.Length-1; i++)
+            {
+                adat[i] = new cFel3(lines[i + 1]);
+            }
+            // B feladat
+            Console.WriteLine($"B feladat: {adat.Count(sor => sor.rgb.SequenceEqual(adat[0].rgb) && sor.IsMirror(adat[0], sizes))}");
+            foreach (cFel3 sor in adat)
+            {
+                if ()
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ 2025-2026/Magyar/irodalom/nyugat.md
@@ -0,0 +1,40 @@
+# Herczeg Ferenc (1863-1954)  
+## Tétel: Herczeg Ferenc a publicista  
+## Fekete szüret a Badacsonyon (Szgy. 229.)  
+1. Műfaj: **Tárca**/tárcanovella  
+	1. Újságírói műfaj  
+	2. Közérdekű esemény/történés a témája  
+	3. Sokszor társadalmi szinten fogalmaz meg kérdéseket  
+	4. Figyelemfelkeltő írás  
+	5. Esszére hasonlít  
+		1. Könnyed stílus  
+		2. Egyéni hangvétel  
+# A nyugat  
+- 1908-ban alapított irodalmi folyóirat  
+- Havonta jelent meg  
+- 1941-ig, Babits Mihály haláláig létezett  
+- Célját a cím is sugallja: A nyugati kultúra és irodalom felé tekintés (főleg francia)  
+	- Pl. stílusirányzatok, modern szemléletek az irodalomban  
+	- De fontosnak tartották a klasszikus, főleg magyar irodalmi hagyományokat  
+- A lap fő szerkesztői:  
+	- Kezdetben:  
+		- Ignotus Pál  
+		- Fenyő Miksa  
+		- Osváth Ernő  
+	- 1920as évek közepétől:  
+		- Babits Mihály  
+		- Móricz Zsigmond  
+- 4 nemzedéket is megkülönböztet  
+	1. Babits Mihály, Móricz Zsigmond, Ady Endre, Kosztolányi Dezső, Tóth Á., Juhász Gy., Kafka Margit, Karinthy  
+	2. József Attila, Szabó Lőrinc, Tamási Áron  
+	3. Radnoti M., Weöres Sándor  
+	4. Átmeneti: Pilinszky János  
+- Hatása  
+	1. Szellemisége tovább él a lapnak, pl.: Magyar Csillag, Újhold c. folyóiratok  
+	2. Megteremtett egy magas irodalmi kultúrát, ami tovább élt  
+- A Nyugat előzménye: a Holnap c. nagyváradi irodalmi lap  
+- A Nyugat mellett más irodalmi lapok  
+	- Herczeg Ferenc: Új idők  
+	- Kassák Lajos: Tett, Ma  
+	- Németh László: Válasz  
+	- Móricz Zsigmond: Kelet Népe  
